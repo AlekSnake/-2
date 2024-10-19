8f0f78ab39c735ccb15d21cc09aff09cc8d284dd
AlekSnake <alexunderas16@gmail.com>
2024-10-20 02:29:10 +0700

    123
    1234

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Press the green button in the gutter to run the script.
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("hello Kate Yoker")
+"1st program"
+print(9 ** 0.5 *5)
+"2nd program"
+print(9.99 > 9.98,  1000 != 1000.1)
+"3rd program"
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+"4th program"
+print(int(float(123.456)*10%10))
